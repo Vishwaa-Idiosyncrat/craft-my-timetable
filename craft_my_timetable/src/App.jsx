991f5cbcb6df47f49f7a782ab53f35c02a218f80
Ananthakrishnan <krishnanthulsiraman2005@gmail.com>
2024-03-09 20:56:39 +0530

    updates

--- craft_my_timetable/src/App.jsx
+++ craft_my_timetable/src/App.jsx
@@ -2,73 +2,71 @@ import axios from "axios";
 
 import React, { Component } from "react";
 
-import "./CourseSelector"
 import MultipleSelectCheckmarks from "./CourseSelector";
-import "./BatchSelector"
 import BatchSelect from "./BatchSelector";
-import "./BranchSelector"
 import BranchSelect from "./BranchSelector";
+import InputFileUpload from "./fileUpload";
 
 class App extends Component {
-  state = {
-    // Initially, no file is selected
-    selectedFile: null,
-  };
+  // state = {
+  //   // Initially, no file is selected
+  //   selectedFile: null,
+  // };
 
-  // On file select (from the pop up)
-  onFileChange = (event) => {
-    // Update the state
-    this.setState({
-      selectedFile: event.target.files[0],
-    });
-  };
+  // // On file select (from the pop up)
+  // onFileChange = (event) => {
+  //   // Update the state
+  //   this.setState({
+  //     selectedFile: event.target.files[0],
+  //   });
+  // };
 
-  // On file upload (click the upload button)
-  onFileUpload = () => {
-    // Create an object of formData
-    const formData = new FormData();
+  // // On file upload (click the upload button)
+  // onFileUpload = () => {
+  //   // Create an object of formData
+  //   const formData = new FormData();
 
-    // Update the formData object
-    formData.append(
-      "myFile",
-      this.state.selectedFile,
-      this.state.selectedFile.name
-    );
+  //   // Update the formData object
+  //   formData.append(
+  //     "myFile",
+  //     this.state.selectedFile,
+  //     this.state.selectedFile.name
+  //   );
 
-    // Details of the uploaded file
-    console.log(this.state.selectedFile);
+  //   // Details of the uploaded file
+  //   console.log(this.state.selectedFile);
 
-    // Request made to the backend api
-    // Send formData object
-    axios.post("api/uploadfile", formData);
-  };
+  //   // Request made to the backend api
+  //   // Send formData object
+  //   axios.post("api/uploadfile", formData);
+  // };
 
-  // File content to be displayed after
-  // file upload is complete
-  fileData = () => {
-    if (this.state.selectedFile) {
-      return (
-        <div>
-          <h2>File Details:</h2>
-          <p>File Name: {this.state.selectedFile.name}</p>
+  // // File content to be displayed after
+  // // file upload is complete
+  // fileData = () => {
+  //   if (this.state.selectedFile) {
+  //     return (
+  //       <div>
+  //         <h2>File Details:</h2>
+  //         <p>File Name: {this.state.selectedFile.name}</p>
 
-          <p>File Type: {this.state.selectedFile.type}</p>
+  //         <p>File Type: {this.state.selectedFile.type}</p>
 
-          <p>
-            Last Modified:{" "}
-            {this.state.selectedFile.lastModifiedDate.toDateString()}
-          </p>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <br />
-          <h4>Choose before Pressing the Upload button</h4>
-        </div>
-      );
-    }
-  };
+  //         <p>
+  //           Last Modified:{" "}
+  //           {this.state.selectedFile.lastModifiedDate.toDateString()}
+  //         </p>
+  //       </div>
+  //     );
+  //   } else {
+  //     return (
+  //       <div>
+  //         <br />
+  //         <h4>Choose before Pressing the Upload button</h4>
+  //       </div>
+  //     );
+  //   }
+  // };
 
   // BatchSelector = () => {
   //   let Batches = [
@@ -88,7 +86,7 @@ class App extends Component {
   //       <h3>Batch</h3>
   //       <form action="/create">
   //         <select className="Batches" id="Batches">
-  //           {Batches.map((item) => 
+  //           {Batches.map((item) =>
   //           <option value={item} onClick={() => console.log(item)}>
   //             {item}
   //           </option>)}
@@ -97,7 +95,6 @@ class App extends Component {
   //     </div>
   //   );
   // };
-
 
   // BranchSelector = () => {
   //   let Branches = [
@@ -113,7 +110,7 @@ class App extends Component {
   //       <h3>Branch</h3>
   //       <form action="/create">
   //         <select className="Branches" id="Branches">
-  //           {Branches.map((item) => 
+  //           {Branches.map((item) =>
   //           <option value={item} onClick={() => console.log(item)}>
   //             {item}
   //           </option>)}
@@ -154,20 +151,15 @@ class App extends Component {
   //   );
   // };
 
-
   render() {
     return (
       <div>
         <h1>Upload the time table</h1>
         <h3>File Upload using React</h3>
-        <div>
-          <input type="file" onChange={this.onFileChange} />
-          <button onClick={this.onFileUpload}>Upload!</button>
-        </div>
-        {this.fileData()}
-        <BatchSelect/>
-        <BranchSelect/>
-        <MultipleSelectCheckmarks/>
+        <InputFileUpload />
+        <BatchSelect />
+        <BranchSelect />
+        <MultipleSelectCheckmarks />
       </div>
     );
   }
