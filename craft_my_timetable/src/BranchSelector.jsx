2e56389afb6aee00227cf93e4b748a6d7a9033dc
Your Name <vishwaaps3@gmail.com>
2024-03-14 07:32:39 +0530

    update

--- craft_my_timetable/src/BranchSelector.jsx
+++ craft_my_timetable/src/BranchSelector.jsx
@@ -1,45 +1,39 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 export default function BranchSelect() {
-  const [Batch, setBatch] = React.useState('');
+  const [Batch, setBatch] = React.useState("");
 
-  const Branches = [
-    "EE",
-    "CSE",
-    "CE",
-    "DS",
-    "ME"
-  ];
+  const Branches = ["CE", "CSE", "EE", "ME", "DS"];
   const handleChange = (event) => {
     setBatch(event.target.value);
   };
 
   return (
     <div>
-        <div>
-            <h3>Please select you Branch</h3>
-        </div>
-        <Box sx={{ minWidth: 300 }}>
+      <div>
+        <h3>Please select you Branch</h3>
+      </div>
+      <Box sx={{ minWidth: 300 }}>
         <FormControl sx={{ m: 1, width: 300 }}>
-            <InputLabel id="branch-select">Branch</InputLabel>
-            <Select
+          <InputLabel id="branch-select">Branch</InputLabel>
+          <Select
             labelId="branch-select"
             id="branch-select"
             value={Batch}
             label="Branch"
             onChange={handleChange}
-            >
-                {Branches.map((item)=>
-            <MenuItem value={item}>
-                {item}</MenuItem>)}
-            </Select>
+          >
+            {Branches.map((item) => (
+              <MenuItem value={item}>{item}</MenuItem>
+            ))}
+          </Select>
         </FormControl>
-        </Box>
+      </Box>
     </div>
   );
 }
